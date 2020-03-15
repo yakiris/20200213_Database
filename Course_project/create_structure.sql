16649430605ed970ff3ad7adcd6ba0e18127f9c1
Irina Yakupova <yakiris@yandex.ru>
2020-03-15 22:00:10 +0300

    course_project

---
+++ Course_project/create_structure.sql
@@ -0,0 +1,155 @@
+CREATE DATABASE receptmira;
+USE receptmira;
+
+CREATE TABLE categories (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	portion INT UNSIGNED NOT NULL
+) COMMENT = 'Категории блюд';
+
+CREATE TABLE continent (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	name VARCHAR(255) NOT NULL
+) COMMENT = 'Континенты';
+
+CREATE TABLE country (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	continent_id INT UNSIGNED NOT NULL
+) COMMENT = 'Страны';
+
+ALTER TABLE country 
+	ADD CONSTRAINT country_continent_id_fk 
+		FOREIGN KEY (continent_id) REFERENCES continent(id);
+	
+CREATE TABLE foods (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	calories FLOAT(5, 1) NOT NULL,
+	protein FLOAT(5, 1) NOT NULL,
+	fats FLOAT(5, 1) NOT NULL,
+	carb FLOAT(5, 1) NOT NULL
+) COMMENT = 'Продукты';
+
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(100) NOT NULL,
+  email VARCHAR(120) NOT NULL UNIQUE 
+)COMMENT = 'Пользователи';
+
+CREATE TABLE profiles (
+  user_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,	
+  gender CHAR(1) NOT NULL,
+  birthday DATE,
+  country_id INT UNSIGNED NOT NULL,
+  photo_id INT UNSIGNED,
+  created DATETIME DEFAULT NOW(),
+  updated DATETIME DEFAULT NOW() ON UPDATE NOW()  
+)COMMENT = 'Профиль пользователя';
+
+ALTER TABLE profiles 
+ADD CONSTRAINT profiles_user_id_fk 
+	FOREIGN KEY (user_id) REFERENCES users(id),
+ADD CONSTRAINT profiles_country_id_fk 
+	FOREIGN KEY (country_id) REFERENCES country(id),
+ADD CONSTRAINT profiles_photo_id_fk 
+	FOREIGN KEY (photo_id) REFERENCES photo(id);
+
+CREATE TABLE photo (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	user_id INT UNSIGNED NOT NULL,
+	filename VARCHAR(255) NOT NULL,
+	filesize INT NOT NULL,
+	created DATETIME DEFAULT NOW()
+)COMMENT = 'Каталог фото';
+
+ALTER TABLE photo 
+ADD CONSTRAINT photo_user_id_fk 
+	FOREIGN KEY (user_id) REFERENCES users(id);
+
+CREATE TABLE recipes (
+id INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255) NOT NULL, 
+country_id INT UNSIGNED NOT NULL, 
+category_id INT UNSIGNED NOT NULL, 
+user_id INT UNSIGNED NOT NULL, 
+process_cook TEXT, 
+time_cook TIME DEFAULT '00:00:00',
+photo_id INT UNSIGNED UNSIGNED DEFAULT 0, 
+created DATETIME DEFAULT NOW(),
+updated DATETIME DEFAULT NOW() ON UPDATE NOW() 
+)COMMENT = 'Каталог рецептов';
+
+ALTER TABLE recipes
+ADD CONSTRAINT recipes_country_id_fk 
+	FOREIGN KEY (country_id) REFERENCES country(id),
+ADD CONSTRAINT recipes_category_id_fk 
+	FOREIGN KEY (category_id) REFERENCES categories(id),
+ADD CONSTRAINT recipes_user_id_fk 
+	FOREIGN KEY (user_id) REFERENCES users(id),
+ADD CONSTRAINT recipes_photo_id_fk 
+	FOREIGN KEY (photo_id) REFERENCES photo(id);
+
+CREATE TABLE ingredients (
+recipes_id INT UNSIGNED NOT NULL,
+food_id INT UNSIGNED NOT NULL,
+quantity INT UNSIGNED NOT NULL
+)COMMENT = 'Таблица расчета ингридиентов';
+
+ALTER TABLE ingredients
+ADD CONSTRAINT ingredients_recipes_id_fk 
+	FOREIGN KEY (recipes_id) REFERENCES recipes(id),
+ADD CONSTRAINT ingredients_food_id_fk 
+	FOREIGN KEY (food_id) REFERENCES foods(id);
+
+CREATE TABLE statuses_friend (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+ )COMMENT = 'Статусы друзей';
+
+CREATE TABLE friends (
+  user_id INT UNSIGNED NOT NULL,
+  friend_id INT UNSIGNED NOT NULL,
+  status_id INT UNSIGNED NOT NULL,
+  created DATETIME DEFAULT NOW(),
+  updated DATETIME DEFAULT NOW() ON UPDATE NOW()
+)COMMENT = 'Друзья';
+
+ALTER TABLE friends
+ADD CONSTRAINT friends_friend_id_fk 
+	FOREIGN KEY (friend_id) REFERENCES users(id),
+ADD CONSTRAINT friends_user_id_fk 
+	FOREIGN KEY (user_id) REFERENCES users(id),
+ADD CONSTRAINT friends_ustatus_id_fk 
+	FOREIGN KEY (status_id) REFERENCES friend_statuses(id);
+
+CREATE TABLE messages (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  from_user_id INT unsigned NOT NULL,
+  to_user_id INT unsigned NOT NULL,
+  body TEXT NOT NULL,
+  is_important TINYINT(1) DEFAULT NULL,
+  is_delivered TINYINT(1) DEFAULT NULL,
+  created_at DATETIME DEFAULT NOW()
+)COMMENT = 'Каталог сообщений';
+
+ALTER TABLE messages
+ADD CONSTRAINT messages_from_user_id_fk 
+	FOREIGN KEY (from_user_id) REFERENCES users(id),
+ADD CONSTRAINT messages_to_user_id_fk 
+	FOREIGN KEY (to_user_id) REFERENCES users(id);
+
+CREATE TABLE likes (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED NOT NULL,
+  pecipe_id INT UNSIGNED NOT NULL,
+  created_at DATETIME DEFAULT NOW()
+)COMMENT = 'Таблица лайков';
+
+ALTER TABLE likes
+ADD CONSTRAINT likes_user_id_fk 
+	FOREIGN KEY (user_id) REFERENCES users(id),
+ADD CONSTRAINT likes_pecipe_id_fk 
+	FOREIGN KEY (pecipe_id) REFERENCES recipes(id);
+
+
---
+++ Course_project/select_data.sql
@@ -0,0 +1,107 @@
+USE receptmira;
+
+-- список рецептов без содержания сахара
+SELECT id,name FROM recipes WHERE id NOT IN 
+	(SELECT r.id FROM recipes r
+		LEFT JOIN ingredients i ON r.id = i.recipes_id
+		LEFT JOIN foods f ON i.food_id = f.id
+	WHERE
+		f.name RLIKE ('^Сахар.*'))
+ORDER BY 
+	id;
+
+-- рецепты с посленим лайком друзей юзера 2
+SELECT u.username, 
+	l.user_id, 
+	r.name recipe, 
+	max(l.created_at) created_at
+FROM likes l 
+	JOIN friends f 
+	JOIN users u	
+		ON (u.id = f.friend_id OR u.id = f.user_id)
+        AND (l.user_id = f.friend_id OR l.user_id = f.user_id)
+    JOIN recipes r ON l.pecipe_id = r.id 
+WHERE u.id = 2 AND l.user_id != 2
+GROUP BY 
+	l.user_id
+ORDER BY 
+	created_at DESC;
+
+-- 10 самых низкалорийных рецептов из категории Суп
+SELECT c.name, 
+	r.name,
+	round(sum(f.calories * i.quantity)/sum(i.quantity), 1) AS calories,
+	round(sum(f.protein * i.quantity)/sum(i.quantity), 1) AS protein,
+	round(sum(f.fats * i.quantity)/sum(i.quantity), 1) AS fats,
+	round(sum(f.carb * i.quantity)/sum(i.quantity), 1) AS carb
+FROM ingredients i 
+	JOIN foods f ON i.food_id = f.id
+	JOIN recipes r ON i.recipes_id = r.id
+	JOIN categories c ON r.category_id = c.id
+WHERE 
+	c.id = 1
+GROUP BY 
+	r.name
+ORDER BY 
+	calories
+LIMIT 10;
+
+-- пол пользователей разместивших 10 самых белковых блюд
+SELECT DISTINCT r.name, 
+	round(sum(f.protein * i.quantity)/sum(i.quantity), 1) protein,
+	(CASE (p.gender) WHEN "m" THEN "мужчина" WHEN "w" THEN "женщина" END) gender
+	FROM recipes r
+		JOIN ingredients i ON r.id = i.recipes_id
+		JOIN foods f ON i.food_id = f.id
+		JOIN profiles p ON r.user_id = p.user_id
+	GROUP BY
+		 r.id
+	ORDER BY
+		protein DESC
+	LIMIT 10;
+	
+-- пользователи лайкающие рецепты своей страны
+SELECT u.username, 
+	c.name, 
+	count(l.id) likes 
+FROM users u
+	JOIN profiles p ON u.id = p.user_id
+	JOIN likes l ON u.id = l.user_id
+	JOIN recipes r ON l.pecipe_id = r.id
+	JOIN country c ON p.country_id = c.id 
+WHERE 
+	p.country_id = r.country_id
+GROUP BY 
+	u.username
+ORDER BY 
+	likes DESC;
+
+-- составление меню с выборкой рецептов с расчетом порций
+CREATE TEMPORARY TABLE menu
+	SELECT r.id id, 
+		r.name recipe,
+		round(sum(i.quantity)/c.portion, 1) portion
+	FROM 
+		recipes r
+ 		JOIN ingredients i ON r.id = i.recipes_id
+ 		JOIN categories c ON r.category_id = c.id
+	WHERE 
+		r.id IN (49, 50, 78, 115)
+	GROUP BY 
+		r.name;
+
+-- составление листа покупок с суммированием количества продуктов на 4 порции на выбранные рецепты
+SELECT  f.name, 
+	round(sum(i.quantity / m.portion * 4)) total 
+FROM ingredients i
+	JOIN menu m ON i.recipes_id = m.id
+	JOIN foods f ON i.food_id = f.id
+ GROUP BY
+	 i.food_id
+ORDER BY 
+	f.name;
+
+-- удаление временной таблицы
+DROP TABLE menu;
+
+
