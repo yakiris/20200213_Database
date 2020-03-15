e036a7277cc3784e4050ef5a67a2b608df0f42f5
Irina Yakupova <yakiris@yandex.ru>
2020-03-15 14:54:54 +0300

    update 07_dz

--- 07_dz/03.sql
+++ 07_dz/03.sql
@@ -1,6 +1,8 @@
 # 3. Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). 
 #Поля from, to и label содержат английские названия городов, поле name — русское. Выведите список рейсов flights с русскими названиями городов.
 
+USE les03;
+
 CREATE TABLE cities (
 lable VARCHAR(255) NOT NULL,
 name VARCHAR(255) NOT NULL
@@ -26,8 +28,18 @@ INSERT INTO flights (frоm, tо) VALUES
 ('omck', 'irkutsk'),
 ('moscow', 'kazan');
 
+-- вариант со вложенными запросами
 SELECT 
 	id,
 	(SELECT name FROM cities WHERE lable = f.frоm) frоm,
     (SELECT name FROM cities WHERE lable = f.tо) tо
- FROM flights f;
+FROM flights f;
+
+-- вариант с JOIN
+SELECT 
+	id,
+	c_from.name frоm,
+    c_to.name tо
+ FROM flights f
+	LEFT JOIN cities c_from ON f.frоm = c_from.lable
+	LEFT JOIN cities c_to ON f.tо = c_to.lable;
